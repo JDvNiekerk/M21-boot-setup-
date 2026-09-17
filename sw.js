ae54111588c5e9204ceeb9712e40759b93c160c5
JDvNiekerk <zprbcy2tjw@privaterelay.appleid.com>
2026-09-17 17:42:02 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,1 +1,82 @@
-const C='m21-v1';self.addEventListener('install',e=>e.waitUntil(caches.open(C).then(c=>c.addAll(['./','./index.html','./manifest.webmanifest']))));self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
+const CACHE_NAME = "m21-boat-setup-v3";
+
+const FILES = [
+  "./",
+  "./index.html",
+  "./manifest.webmanifest",
+  "./sw.js"
+];
+
+self.addEventListener("install", event => {
+
+  event.waitUntil(
+
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(FILES))
+
+  );
+
+  self.skipWaiting();
+
+});
+
+
+self.addEventListener("activate", event => {
+
+  event.waitUntil(
+
+    caches.keys().then(keys =>
+
+      Promise.all(
+
+        keys
+          .filter(key => key !== CACHE_NAME)
+          .map(key => caches.delete(key))
+
+      )
+
+    )
+
+  );
+
+  self.clients.claim();
+
+});
+
+
+self.addEventListener("fetch", event => {
+
+  if(event.request.method !== "GET")
+    return;
+
+  event.respondWith(
+
+    fetch(event.request)
+      .then(response => {
+
+        if(response && response.ok){
+
+          const copy = response.clone();
+
+          caches.open(CACHE_NAME)
+            .then(cache =>
+              cache.put(event.request,copy)
+            );
+
+        }
+
+        return response;
+
+      })
+      .catch(() =>
+
+        caches.match(event.request)
+          .then(cached =>
+            cached || caches.match("./index.html")
+          )
+
+      )
+
+  );
+
+});
